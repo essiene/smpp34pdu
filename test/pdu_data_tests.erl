4b856a604723fa9e3279273e30574db813c17720
Essien Ita Essien <essiene@gmail.com>
2010-06-17 23:35:35 +0100

    Add unittests for pdu_data:string_to_bin
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

---
+++ test/pdu_data_tests.erl
@@ -0,0 +1,17 @@
+-module(pdu_data_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+string_to_bin_test_() ->
+	[
+		{"A string less than MAX will convert", 
+			?_assert(<<"foo!">> == pdu_data:string_to_bin("foo!", 4))},
+		{"A string less than max will pass but truncated",
+			?_assert(<<"fo">> == pdu_data:string_to_bin("foo!", 2))},
+		{"No function clause for non strings",
+    		?_assertError(function_clause, pdu_data:string_to_bin(1, 4))},
+		{"The atom 'undefined' will yield an empty binary",
+    		?_assert(<<>> == pdu_data:string_to_bin(undefined, 8))},
+		{"An empty string will yield empty binary",
+			?_assert(<<>> == pdu_data:string_to_bin("", 8))}
+	].
+
