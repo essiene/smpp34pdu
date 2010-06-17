0addcc94a1b946318b62cd1f758f0461963c3257
Essien Ita Essien <essiene@gmail.com>
2010-06-17 23:41:31 +0100

    Unittest cstring_to_bin
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- test/pdu_data_tests.erl
+++ test/pdu_data_tests.erl
@@ -15,3 +15,18 @@ string_to_bin_test_() ->
 			?_assert(<<>> == pdu_data:string_to_bin("", 8))}
 	].
 
+cstring_to_bin_test_() ->
+	[
+		{"A string less than MAX will convert", 
+			?_assert(<<102,111,111,0>> == pdu_data:cstring_to_bin("foo", 4))},
+		{"A string less than max will pass but truncated",
+			?_assert(<<102,0>> == pdu_data:cstring_to_bin("foo!", 2))},
+		{"No function clause for non strings",
+    		?_assertError(function_clause, pdu_data:cstring_to_bin(1, 4))},
+		{"The atom 'undefined' will yield null terminated, empty binary",
+    		?_assert(<<0>> == pdu_data:cstring_to_bin(undefined, 8))},
+		{"An empty string will yield null terminated, empty binary",
+			?_assert(<<0>> == pdu_data:cstring_to_bin("", 8))}
+	].
+
+
