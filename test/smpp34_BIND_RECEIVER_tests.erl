abcf814c609981f525f0dd1aea6f048ced7d6964
Essien Ita Essien <essiene@gmail.com>
2010-06-19 17:13:03 +0100

    Update BIND_RECEIVER unittests
    - pack only packs the #bind_receiver{} pdu
    - unpack only unpacks into #bind_receiver{} record
    - if we pack and unpack we should get back the same thing.
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- test/smpp34_BIND_RECEIVER_tests.erl
+++ test/smpp34_BIND_RECEIVER_tests.erl
@@ -9,14 +9,32 @@ bind_receiver_pack_test_() ->
         addr_ton=2,
         addr_npi=1},
 
-    PduBin = <<0,0,0,37,0,0,0,1,0,0,0,0,0,0,0,1,97,
-	           98,99,100,101,102,103,104,105,106,0,
-			   97,98,99,100,0,0,52,2,1,0>>,
+    Bin = <<97,98,99,100,101,102,103,104,105,106,0,97,98,99,100,0,0,52,2,1,0>>,
 
-	Pdu = #pdu {command_id=?BIND_RECEIVER,
-			    sequence_number=1,
-				body=Payload},
+	Packed = smpp34_BIND_RECEIVER:pack(Payload),
+
+	?_assertEqual(Bin,Packed).
+
+bind_receiver_unpack_test_() -> 
+    Payload = #bind_receiver{system_id="abcdefghij",
+        password="abcd",
+		system_type="",
+		interface_version=?VERSION,
+        addr_ton=2,
+        addr_npi=1,
+		address_range=""},
+
+    Bin = <<97,98,99,100,101,102,103,104,105,106,0,97,98,99,100,0,0,52,2,1,0>>,
 	
-	Packed = smpp34_BIND_RECEIVER:pack(Pdu),
+	UnPacked = smpp34_BIND_RECEIVER:unpack(Bin),
 
-	[?_assertEqual(PduBin,Packed)].
+	?_assertEqual(Payload,UnPacked).
+
+cyclic_test_() ->
+    Payload = #bind_receiver{system_id="abcdefghij",
+        password="abcd",
+        addr_ton=2,
+        addr_npi=1},
+
+	?_assertEqual(Payload, smpp34_BIND_RECEIVER:unpack(smpp34_BIND_RECEIVER:pack(Payload))).
+
