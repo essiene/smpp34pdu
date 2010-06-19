c0f31f6e68bb86e1d2a2667f0e0eeab9640a64c9
Essien Ita Essien <essiene@gmail.com>
2010-06-19 17:05:10 +0100

    Pack only the #bind_receiver{} PDU type in here
    The packing of the top level #pdu{} will be done externally
    and module will ONLY be used to pack/unpack a #bind_receiver{}
    pdu
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/smpp34_BIND_RECEIVER.erl
+++ src/smpp34_BIND_RECEIVER.erl
@@ -4,15 +4,13 @@
 -import(pdu_data, [cstring_to_bin/2, integer_to_bin/2]).
 
 
-pack(#pdu{command_id=?BIND_RECEIVER, sequence_number=Snum, body=#bind_receiver{
-			system_id=SystemId,
-			password=Password,
-			system_type=SystemType,
-			interface_version=InterfaceVersion,
-			addr_ton=AddrTon,
-			addr_npi=AddrNpi,
-			address_range=AddressRange
-		}}) ->
+pack(#bind_receiver{system_id=SystemId, 
+		password=Password, 
+		system_type=SystemType, 
+		interface_version=InterfaceVersion, 
+		addr_ton=AddrTon, 
+		addr_npi=AddrNpi, 
+		address_range=AddressRange}) ->
 
 		L = [cstring_to_bin(SystemId, 16),
 					   cstring_to_bin(Password, 9),
@@ -22,12 +20,4 @@ pack(#pdu{command_id=?BIND_RECEIVER, sequence_number=Snum, body=#bind_receiver{
 					   integer_to_bin(AddrNpi, 1),
 					   cstring_to_bin(AddressRange, 41)],
 
-		pack(?BIND_RECEIVER, 0, Snum, L).
-
-pack(Cid, Cstat, Snum, BinList) ->
-		Body = list_to_binary(BinList),
-		Clen = byte_size(Body) + ?HEADER_OCTET_SIZE,
-		Cstat = 0,
-		
-		L = [<<Clen:32,Cid:32,Cstat:32,Snum:32>> | BinList],
 		list_to_binary(L).
