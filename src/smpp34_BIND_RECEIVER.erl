49a88b37ad77bab91a842d2be229778350f97705
Essien Ita Essien <essiene@gmail.com>
2010-06-19 17:24:40 +0100

    Add specificiations for smpp34_BIND_RECEIVER
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/smpp34_BIND_RECEIVER.erl
+++ src/smpp34_BIND_RECEIVER.erl
@@ -1,9 +1,12 @@
 -module(smpp34_BIND_RECEIVER).
 -include("pdu.hrl").
+-include("types.hrl").
 -export([pack/1,unpack/1]).
 -import(pdu_data, [cstring_to_bin/2, integer_to_bin/2]).
 -import(pdu_data, [bin_to_cstring/2, bin_to_integer/2]).
 
+-spec(pack/1 :: (bind_receiver()) -> binary()).
+-spec(unpack/1 :: (binary()) -> bind_receiver()).
 
 pack(#bind_receiver{system_id=SystemId, 
 		password=Password, 
