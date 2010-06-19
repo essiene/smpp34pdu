2a33afd8b116e5ae7141158858d35599157e0d42
Essien Ita Essien <essiene@gmail.com>
2010-06-19 17:06:56 +0100

    Implement #bind_receiver{} unpack
    Also, we only attempt to unpack into the #bind_receiver{} pdu,
    not into the TOP LEVEL #pdu{} record, another module will
    handle the top level container and ONLY call out to this
    module when the command id is ?BIND_RECEIVER
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/smpp34_BIND_RECEIVER.erl
+++ src/smpp34_BIND_RECEIVER.erl
@@ -1,7 +1,8 @@
 -module(smpp34_BIND_RECEIVER).
 -include("pdu.hrl").
--export([pack/1,pack/4]).
+-export([pack/1,unpack/1]).
 -import(pdu_data, [cstring_to_bin/2, integer_to_bin/2]).
+-import(pdu_data, [bin_to_cstring/2, bin_to_integer/2]).
 
 
 pack(#bind_receiver{system_id=SystemId, 
@@ -21,3 +22,22 @@ pack(#bind_receiver{system_id=SystemId,
 					   cstring_to_bin(AddressRange, 41)],
 
 		list_to_binary(L).
+
+
+unpack(Bin0) ->
+	{SystemId, Bin1} = bin_to_cstring(Bin0, 16),
+	{Password, Bin2} = bin_to_cstring(Bin1, 9),
+	{SystemType, Bin3} = bin_to_cstring(Bin2, 13),
+	{InterfaceVersion, Bin4} = bin_to_integer(Bin3, 1),
+	{AddrTon, Bin5} = bin_to_integer(Bin4, 1),
+	{AddrNpi, Bin6} = bin_to_integer(Bin5, 1),
+	{AddressRange, <<>>} = bin_to_cstring(Bin6, 41),
+
+	#bind_receiver {system_id=SystemId,
+		password=Password,
+		system_type=SystemType,
+		interface_version=InterfaceVersion,
+		addr_ton=AddrTon,
+		addr_npi=AddrNpi,
+		address_range=AddressRange}.
+
