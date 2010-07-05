901efd5f7bcd50f31705632b97e2218e9838bcf4
Essien Ita Essien <essiene@gmail.com>
2010-07-05 12:28:49 +0100

    Implement alert_notification PDU module
    Not fully working yet as not all tlv clauses are currently
    implemented.
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

---
+++ src/smpp34pdu_alert_notification.erl
@@ -0,0 +1,50 @@
+-module(smpp34pdu_alert_notification).
+-include("pdu.hrl").
+-include("types.hrl").
+-export([pack/1,unpack/1]).
+-import(pdu_data, [cstring_to_bin/2, integer_to_bin/2]).
+-import(pdu_data, [bin_to_cstring/2, bin_to_integer/2]).
+
+-spec(pack/1 :: (alert_notification()) -> binary()).
+-spec(unpack/1 :: (binary()) -> alert_notification()).
+
+pack(#alert_notification{source_addr_ton=SrcAddrTon,
+		source_addr_npi=SrcAddrNpi,
+		source_addr=SrcAddr,
+		esme_addr_ton=EsmeAddrTon,
+		esme_addr_npi=EsmeAddrNpi,
+		esme_addr=EsmeAddr,
+		ms_availability_status=MsAvailStat}) ->
+
+
+		L = [integer_to_bin(SrcAddrTon, 1),
+					   integer_to_bin(SrcAddrNpi, 1),
+					   cstring_to_bin(SrcAddr, 21),
+					   integer_to_bin(EsmeAddrTon, 1),
+					   integer_to_bin(EsmeAddrNpi, 1),
+					   cstring_to_bin(EsmeAddr, 21),
+					   tlv:pack(?MS_AVAILABILITY_STATUS, MsAvailStat)],
+
+		list_to_binary(L).
+
+
+unpack(Bin0) ->
+	{SrcAddrTon, Bin1} = bin_to_integer(Bin0, 1),
+	{SrcAddrNpi, Bin2} = bin_to_integer(Bin1, 1),
+	{SrcAddr, Bin3} = bin_to_cstring(Bin2, 21),
+	{EsmeAddrTon, Bin4} = bin_to_integer(Bin3, 1),
+	{EsmeAddrNpi, Bin5} = bin_to_integer(Bin4, 1),
+	{EsmeAddr, Bin6} = bin_to_cstring(Bin5, 21),
+
+	unpack_tlv_fields(Bin6, #alert_notification {source_addr_ton=SrcAddrTon,
+		source_addr_npi=SrcAddrNpi,
+		source_addr=SrcAddr,
+		esme_addr_ton=EsmeAddrTon,
+		esme_addr_npi=EsmeAddrNpi,
+		esme_addr=EsmeAddr}).
+
+unpack_tlv_fields(<<>>, Body) ->
+	Body;
+unpack_tlv_fields(<<?MS_AVAILABILITY_STATUS:?TLV_TAG_SIZE, Rest0/binary>>, Body) ->
+	{Val, Rest1} = tlv:unpack(?MS_AVAILABILITY_STATUS, Rest0),
+	unpack_tlv_fields(Rest1, Body#alert_notification{ms_availability_status=Val}).
