b994e4df2512855589fab512248f32ad76e024c5
Essien Ita Essien <essiene@gmail.com>
2010-07-05 09:40:42 +0100

    Unittesting for tlv module
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- test/tlv_tests.erl
+++ test/tlv_tests.erl
@@ -2,20 +2,20 @@
 -include("../src/constants.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
-tlv_test_() ->
+to_bin_test_() ->
 	[
-		{"pack", 
+		{"to_bin", 
 			[{"undefined yields empty binary",
-				?_assertEqual(<<>>, tlv:pack(?SC_INTERFACE_VERSION, undefined))},
+				?_assertEqual(<<>>, tlv:to_bin(?SC_INTERFACE_VERSION, undefined))},
 			 {"sc_interface_version", 
-				?_assertEqual(<<2,16,0,1,52>>, tlv:pack(?SC_INTERFACE_VERSION, 16#34))}
+				?_assertEqual(<<2,16,0,1,52>>, tlv:to_bin(?SC_INTERFACE_VERSION, 16#34))}
 			]
 		},
 
-		{"unpack",
+		{"from_bin",
 			[
 				{"sc_interface_version", 
-					?_assertEqual({16#34, <<>>}, tlv:unpack(?SC_INTERFACE_VERSION, <<0,1,52>>))}
+					?_assertEqual({16#34, <<>>}, tlv:from_bin(?SC_INTERFACE_VERSION, <<0,1,52>>))}
 			]
 		}
 	].
