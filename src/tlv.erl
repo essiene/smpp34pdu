608dd0a96f2963ee437ff47b9d02a07d5ec772b7
Essien Ita Essien <essiene@gmail.com>
2010-07-05 09:41:59 +0100

    Rename to [un]pack
    Rename the top-level tlv functions to pack/2 and unpack/2
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/tlv.erl
+++ src/tlv.erl
@@ -1,16 +1,16 @@
 -module(tlv).
 -include("constants.hrl").
--export[to_bin/2, from_bin/2].
+-export[pack/2, unpack/2].
 
--spec(to_bin/2 :: (integer(), integer()) -> binary()).
--spec(from_bin/2 :: (integer(), binary()) -> {integer(), binary()}).
+-spec(pack/2 :: (integer(), integer()) -> binary()).
+-spec(unpack/2 :: (integer(), binary()) -> {integer(), binary()}).
 
-to_bin(_, undefined) ->
+pack(_, undefined) ->
 	<<>>;
-to_bin(?SC_INTERFACE_VERSION, Val) ->
+pack(?SC_INTERFACE_VERSION, Val) ->
 	Len = 1,
 	Size = Len * ?OCTET_SIZE,
 	<<?SC_INTERFACE_VERSION:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE, Val:Size>>.
 
-from_bin(?SC_INTERFACE_VERSION, <<Len:16,Rest0/binary>>) ->
+unpack(?SC_INTERFACE_VERSION, <<Len:16,Rest0/binary>>) ->
 	pdu_data:bin_to_integer(Rest0, Len).
