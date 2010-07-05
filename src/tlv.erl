d48ea527bc64ae42579514c4b718152253d9e443
Essien Ita Essien <essiene@gmail.com>
2010-07-05 12:34:58 +0100

    Implement tlv [un]packing for MS_AVAILABILITY_STATUS
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/tlv.erl
+++ src/tlv.erl
@@ -10,7 +10,15 @@ pack(_, undefined) ->
 pack(?SC_INTERFACE_VERSION, Val) ->
 	Len = 1,
 	Size = Len * ?OCTET_SIZE,
-	<<?SC_INTERFACE_VERSION:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE, Val:Size>>.
+	<<?SC_INTERFACE_VERSION:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE, Val:Size>>;
+
+pack(?MS_AVAILABILITY_STATUS, Val) ->
+	Len = 1,
+	Size = Len * ?OCTET_SIZE,
+	<<?MS_AVAILABILITY_STATUS:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE, Val:Size>>.
 
 unpack(?SC_INTERFACE_VERSION, <<Len:16,Rest0/binary>>) ->
+	pdu_data:bin_to_integer(Rest0, Len);
+
+unpack(?MS_AVAILABILITY_STATUS, <<Len:16,Rest0/binary>>) ->
 	pdu_data:bin_to_integer(Rest0, Len).
