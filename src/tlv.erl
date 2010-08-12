2e5c2b0180de110329c911e442a4f2fbddaeb255
Essien Ita Essien <essiene@gmail.com>
2010-08-12 12:07:57 +0100

    SAR_TOTAL_SEGMENTS and SAR_SEGMENT_SEQNUM support
    - complete with unittests
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/tlv.erl
+++ src/tlv.erl
@@ -28,6 +28,106 @@ pack(?SOURCE_NETWORK_TYPE, Val) ->
 	Size = Len * ?OCTET_SIZE,
 	<<?SOURCE_NETWORK_TYPE:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE, Val:Size>>;
 
+pack(?DEST_BEARER_TYPE, Val) ->
+	Len = 1,
+	Size = Len * ?OCTET_SIZE,
+	<<?DEST_BEARER_TYPE:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE, Val:Size>>;
+
+pack(?SOURCE_BEARER_TYPE, Val) ->
+	Len = 1,
+	Size = Len * ?OCTET_SIZE,
+	<<?SOURCE_BEARER_TYPE:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE, Val:Size>>;
+
+pack(?DEST_TELEMATICS_ID, Val) ->
+	Len = 2,
+	Size = Len * ?OCTET_SIZE,
+	<<?DEST_TELEMATICS_ID:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE, Val:Size>>;
+
+pack(?SOURCE_TELEMATICS_ID, Val) ->
+	Len = 1,
+	Size = Len * ?OCTET_SIZE,
+	<<?SOURCE_TELEMATICS_ID:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE, Val:Size>>;
+
+pack(?QOS_TIME_TO_LIVE, Val) ->
+	Len = 4,
+	Size = Len * ?OCTET_SIZE,
+	<<?QOS_TIME_TO_LIVE:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE, Val:Size>>;
+
+pack(?PAYLOAD_TYPE, Val) ->
+	Len = 1,
+	Size = Len * ?OCTET_SIZE,
+	<<?PAYLOAD_TYPE:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE, Val:Size>>;
+
+pack(?ADDITIONAL_STATUS_INFO_TEXT, Val) ->
+	Len = length(Val) + 1,
+	L = [<<?ADDITIONAL_STATUS_INFO_TEXT:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE>>, pdu_data:cstring_to_bin(Val,256)],
+	list_to_binary(L);
+
+pack(?RECEIPTED_MESSAGE_ID, Val) ->
+	Len = length(Val) + 1,
+	L = [<<?RECEIPTED_MESSAGE_ID:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE>>, pdu_data:cstring_to_bin(Val,65)],
+	list_to_binary(L);
+
+pack(?MS_MSG_WAIT_FACILITIES, Val) ->
+	Len = 1,
+	Size = Len * ?OCTET_SIZE,
+	<<?MS_MSG_WAIT_FACILITIES:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE, Val:Size>>;
+
+pack(?PRIVACY_INDICATOR, Val) ->
+	Len = 1,
+	Size = Len * ?OCTET_SIZE,
+	<<?PRIVACY_INDICATOR:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE, Val:Size>>;
+
+pack(?SOURCE_SUBADDRESS, Val) ->
+	Len = byte_size(Val),
+	L = [<<?SOURCE_SUBADDRESS:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE>>, pdu_data:octstring_to_bin(Val,23)], % minimum of 2, should be implemented
+	list_to_binary(L);
+
+pack(?DEST_SUBADDRESS, Val) ->
+	Len = byte_size(Val),
+	L = [<<?DEST_SUBADDRESS:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE>>, pdu_data:octstring_to_bin(Val,23)], % minimum of 2, should be implemented
+	list_to_binary(L);
+
+pack(?USER_MESSAGE_REFERENCE, Val) ->
+	Len = 2,
+	Size = Len * ?OCTET_SIZE,
+	<<?USER_MESSAGE_REFERENCE:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE, Val:Size>>;
+
+pack(?USER_RESPONSE_CODE, Val) ->
+	Len = 1,
+	Size = Len * ?OCTET_SIZE,
+	<<?USER_RESPONSE_CODE:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE, Val:Size>>;
+
+pack(?LANGUAGE_INDICATOR, Val) ->
+	Len = 1,
+	Size = Len * ?OCTET_SIZE,
+	<<?LANGUAGE_INDICATOR:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE, Val:Size>>;
+
+pack(?SOURCE_PORT, Val) ->
+	Len = 2,
+	Size = Len * ?OCTET_SIZE,
+	<<?SOURCE_PORT:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE, Val:Size>>;
+
+pack(?DESTINATION_PORT, Val) ->
+	Len = 2,
+	Size = Len * ?OCTET_SIZE,
+	<<?DESTINATION_PORT:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE, Val:Size>>;
+
+pack(?SAR_MSG_REF_NUM, Val) ->
+	Len = 2,
+	Size = Len * ?OCTET_SIZE,
+	<<?SAR_MSG_REF_NUM:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE, Val:Size>>;
+
+pack(?SAR_TOTAL_SEGMENTS, Val) ->
+	Len = 1,
+	Size = Len * ?OCTET_SIZE,
+	<<?SAR_TOTAL_SEGMENTS:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE, Val:Size>>;
+
+pack(?SAR_SEGMENT_SEQNUM, Val) ->
+	Len = 1,
+	Size = Len * ?OCTET_SIZE,
+	<<?SAR_SEGMENT_SEQNUM:?TLV_TAG_SIZE, Len:?TLV_LEN_SIZE, Val:Size>>;
+
 pack(?SC_INTERFACE_VERSION, Val) ->
 	Len = 1,
 	Size = Len * ?OCTET_SIZE,
@@ -69,6 +169,9 @@ unpack(?SAR_TOTAL_SEGMENTS, <<Len:?TLV_LEN_SIZE,Rest0/binary>>) ->
 	pdu_data:bin_to_integer(Rest0, Len);
 
 unpack(?SAR_SEGMENT_SEQNUM, <<Len:?TLV_LEN_SIZE,Rest0/binary>>) ->
+	pdu_data:bin_to_integer(Rest0, Len);
+
+unpack(?SC_INTERFACE_VERSION, <<Len:?TLV_LEN_SIZE,Rest0/binary>>) ->
 	pdu_data:bin_to_integer(Rest0, Len);
 
 unpack(?MORE_MESSAGES_TO_SEND, <<Len:?TLV_LEN_SIZE,Rest0/binary>>) ->
