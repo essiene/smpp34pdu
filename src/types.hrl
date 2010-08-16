ea4b76949ca5d3adb946e2d9eeb3539278c26597
Essien Ita Essien <essiene@gmail.com>
2010-08-16 17:26:46 +0100

    < R13B03 can not forward reference type definitions
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/types.hrl
+++ src/types.hrl
@@ -3,11 +3,31 @@
 
 -include("smpp34pdu.hrl").
 
--type(pdu() :: #pdu{command_length :: integer(),
-					command_id :: integer(),
-					command_status :: integer(),
-					sequence_number :: integer(), 
-					body :: valid_pdu() | valid_pdu_error()}).
+-type(generic_nack() :: #generic_nack{}).
+-type(bind_receiver() :: #bind_receiver{}).
+-type(bind_receiver_resp() :: #bind_receiver_resp{}).
+-type(bind_transmitter() :: #bind_transmitter{}).
+-type(bind_transmitter_resp() :: #bind_transmitter_resp{}).
+-type(query_sm() :: #query_sm{}).
+-type(query_sm_resp() :: #query_sm_resp{}).
+-type(submit_sm() :: #submit_sm{}).
+-type(submit_sm_resp() :: #submit_sm_resp{}).
+-type(deliver_sm_resp() :: #deliver_sm_resp{}).
+-type(unbind() :: #unbind{}).
+-type(unbind_resp() :: #unbind_resp{}).
+-type(replace_sm() :: #replace_sm{}).
+-type(replace_sm_resp() :: #replace_sm_resp{}).
+-type(cancel_sm() :: #cancel_sm{}).
+-type(cancel_sm_resp() :: #cancel_sm_resp{}).
+-type(bind_transceiver() :: #bind_transceiver{}).
+-type(bind_transceiver_resp() :: #bind_transceiver_resp{}).
+-type(outbind() :: #outbind{}).
+-type(enquire_link() :: #enquire_link{}).
+-type(enquire_link_resp() :: #enquire_link_resp{}).
+-type(alert_notification() :: #alert_notification{}).
+
+-type(invalid_command_id() :: {'error', {'command_id', integer()}}).
+
 
 -type(valid_pdu() ::  generic_nack()
 					| bind_receiver()
@@ -34,29 +54,10 @@
 
 -type(valid_pdu_error() :: invalid_command_id()).
 
--type(generic_nack() :: #generic_nack{}).
--type(bind_receiver() :: #bind_receiver{}).
--type(bind_receiver_resp() :: #bind_receiver_resp{}).
--type(bind_transmitter() :: #bind_transmitter{}).
--type(bind_transmitter_resp() :: #bind_transmitter_resp{}).
--type(query_sm() :: #query_sm{}).
--type(query_sm_resp() :: #query_sm_resp{}).
--type(submit_sm() :: #submit_sm{}).
--type(submit_sm_resp() :: #submit_sm_resp{}).
--type(deliver_sm_resp() :: #deliver_sm_resp{}).
--type(unbind() :: #unbind{}).
--type(unbind_resp() :: #unbind_resp{}).
--type(replace_sm() :: #replace_sm{}).
--type(replace_sm_resp() :: #replace_sm_resp{}).
--type(cancel_sm() :: #cancel_sm{}).
--type(cancel_sm_resp() :: #cancel_sm_resp{}).
--type(bind_transceiver() :: #bind_transceiver{}).
--type(bind_transceiver_resp() :: #bind_transceiver_resp{}).
--type(outbind() :: #outbind{}).
--type(enquire_link() :: #enquire_link{}).
--type(enquire_link_resp() :: #enquire_link_resp{}).
--type(alert_notification() :: #alert_notification{}).
-
--type(invalid_command_id() :: {'error', {'command_id', integer()}}).
+-type(pdu() :: #pdu{command_length :: integer(),
+					command_id :: integer(),
+					command_status :: integer(),
+					sequence_number :: integer(), 
+					body :: valid_pdu() | valid_pdu_error()}).
 
 -endif.
