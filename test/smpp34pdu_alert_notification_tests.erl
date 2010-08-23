73f76adba98202f396959a41bcc9a63f8d3c3838
Essien Ita Essien <essiene@gmail.com>
2010-08-23 20:01:37 +0100

    Test that TLV is excluded when not set
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- test/smpp34pdu_alert_notification_tests.erl
+++ test/smpp34pdu_alert_notification_tests.erl
@@ -14,15 +14,22 @@ alert_notification_test_() ->
 
     Bin = <<2,1,97,98,99,100,0,2,1,101,102,103,104,0,4,34,0,1,2>>,
 
+    PayloadNoTlv = #alert_notification{source_addr_ton=2,
+		source_addr_npi=1,
+        source_addr="abcd",
+		esme_addr_ton=2,
+		esme_addr_npi=1,
+		esme_addr="efgh"},
+
+    BinNoTlv = <<2,1,97,98,99,100,0,2,1,101,102,103,104,0>>,
+
 	[
 		{"Packing Payload will give Bin",
 			?_assertEqual(Bin,smpp34pdu_alert_notification:pack(Payload))},
 		{"Unpacking Bin will give Payload",
 			?_assertEqual(Payload, smpp34pdu_alert_notification:unpack(Bin))},
-		{"Packing and Unpacking Payload will give you Payload", 
-			?_assertEqual(Payload,
-					smpp34pdu_alert_notification:unpack(smpp34pdu_alert_notification:pack(Payload)))},
-		{"Unpacking and Packing Bin will give you Bin", 
-			?_assertEqual(Bin,
-					smpp34pdu_alert_notification:pack(smpp34pdu_alert_notification:unpack(Bin)))}
+		{"Packing PayloadNoTlv will give BinNoTlv",
+			?_assertEqual(BinNoTlv,smpp34pdu_alert_notification:pack(PayloadNoTlv))},
+		{"Unpacking BinNoTlv will give PayloadNoTlv",
+			?_assertEqual(PayloadNoTlv, smpp34pdu_alert_notification:unpack(BinNoTlv))}
 	].
