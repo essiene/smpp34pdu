4bcde8acaabe88b6dfb9f677ee746e4e8a8797e1
Essien Ita Essien <essiene@gmail.com>
2010-11-10 23:24:13 +0100

    Start work on tlv_macros.hrl
    This contains a set of macros to eliminate the crappy typing repetition
    currently done in the tlv unpacking sequences.
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

---
+++ src/tlv_macros.hrl
@@ -0,0 +1,13 @@
+-ifndef(tlv_macros).
+-define(tlv_macros, true).
+
+-define(TLV_UNPACK_EMPTY_BIN, unpack_tlv_fields(<<>>, Body) -> Body).
+-define(TLV_UNPACK_UNEXPECTED, unpack_tlv_fields(<<Unexpected:?TLV_TAG_SIZE, _/binary>>=Bin, Body) ->
+    {_, Rest} = tlv:unpack(Unexpected, Bin),
+    unpack_tlv_fields(Rest, Body)).
+-define(TLV_UNPACK_FIELD(RECORD_NAME,RECORD_FIELD,FIELD_TAG), unpack_tlv_fields(<<FIELD_TAG:?TLV_TAG_SIZE, _/binary>>=Bin, Body) ->
+	{Val, Rest} = tlv:unpack(FIELD_TAG, Bin),
+    unpack_tlv_fields(Rest, Body#RECORD_NAME{RECORD_FIELD=Val})).
+
+
+-endif.
